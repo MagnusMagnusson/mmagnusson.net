cd86e9dac91899a7e4a68f008777b474fe35d1db
Magnus_Magnusson <magnusm15@ru.is>
2018-05-16 18:45:38 +0000

    Added static files and a simple template for testing

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns= i18n_patterns(
     url(r'^$',  include('mainSite.urls'),
 ))
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
