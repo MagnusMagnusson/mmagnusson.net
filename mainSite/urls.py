92d21b4d5b77a5aadc373de2a99e8cc4c566c170
Magnus_Magnusson <magnusm15@ru.is>
2018-05-26 02:13:57 +0000

    About hereabout ready

--- mainSite/urls.py
+++ mainSite/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	#HTML REQUESTS
-    url(r'um$', views.about, name='about'),
+    url(r'um$', views.about, name='um'),
     url(r'about$', views.about, name='about'),
     url(r'^(index)?$', views.index, name='index'),
 ] 
