6a7d4c4a20274aeb9b79f4623c88339b308cc672
Magnus_Magnusson <magnusm15@ru.is>
2018-12-30 03:22:29 +0000

    contact me, og eitthvað fleira eldgamalt

--- mainSite/views.py
+++ mainSite/views.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, loader
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
 # Create your views here.
 
-def index(request,self):
+def index(request):
 	template = loader.get_template('mainSite/index.html')
 	context = {}
 	return HttpResponse(template.render(context, request))
@@ -17,3 +19,11 @@ def about(request):
 	context = {}
 	return HttpResponse(template.render(context, request))
 
+
+def contact(request):
+	template = loader.get_template('mainSite/contact.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
+
+def favico(request):
+	return redirect('../static/mainSite/images/favicon.ico')
