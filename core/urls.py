6159f557436f2ae8408e507e313f003700d1d018
Magnus_Magnusson <magnusm15@ru.is>
2018-12-31 21:19:45 +0000

    Matarast sett upp

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns= i18n_patterns(
-    url(r'',  include('mainSite.urls'),
-))
+    url(r'test/',  include('matarast.urls')),
+    url(r'',  include('mainSite.urls')),
+	)
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
