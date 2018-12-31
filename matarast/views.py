949a9bae714f073a917ef24899c730a6a160165b
Magnus_Magnusson <magnusm15@ru.is>
2018-12-31 22:27:19 +0000

    Matarast sett upp

--- matarast/views.py
+++ matarast/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.shortcuts import redirect
 
-# Create your views here.
+
 def index(request):
-	return redirect('www.google.com')
+	template = loader.get_template('matarast/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
