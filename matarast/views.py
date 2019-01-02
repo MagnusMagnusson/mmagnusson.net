1e5232766365515141d10ce43f006a931efc5581
Magnus_Magnusson <magnusm15@ru.is>
2019-01-02 01:11:27 +0000

    lending fyrir matarást

--- matarast/views.py
+++ matarast/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.shortcuts import redirect
 
+from django.shortcuts import render, loader
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
 	template = loader.get_template('matarast/index.html')
